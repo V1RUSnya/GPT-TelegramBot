18434f85d3757e885c071a95bf6beab84ed83b59
Hexqueller <49871848+V1RUSnya@users.noreply.github.com>
2023-10-30 11:27:54 +0300

    Fix lagg and now bot can work

--- bot.py
+++ bot.py
@@ -8,33 +8,25 @@ g4f.debug.logging = True # enable logging
 g4f.check_version = False
 print(g4f.version)
 
-def aiASK(question):
-    response = g4f.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": question}],
-        provider= Yqcloud,
-        stream=False,
-    )
-    return response
-        
-    
- 
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
     bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот с нейросетью gpt 3.5-turbo!\nЗадавайте любые вопросы!".format(message.from_user, bot.get_me()), parse_mode='html')
     
-@bot.message_handler(commands=['test'])
-def test(message):
-    bot.send_message(message.chat.id, "Test")
-    bot.edit_message_text("Новый текст сообщения", chat_id=message.chat_id, message_id=message_id)
-    
 @bot.message_handler(content_types=['text'])
 def ask(message):
-    response = aiASK(message.text)
+    print(f"\nВопрос: {message.text}\n")
+    response = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message.text}],
+        provider= Yqcloud,
+        stream=False,
+    )
+    textCloud = ""
     for messaga in response:
         print(messaga, flush=True, end='')
-        bot.send_message(message.chat.id, str(messaga))
+        textCloud += messaga
+    bot.send_message(message.chat.id, str(textCloud))    
 
 bot.polling(none_stop=True)
